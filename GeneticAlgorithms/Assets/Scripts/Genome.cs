36cde80227f46d5cfb111b1932c800e450d5a11f
Lizi Attwood <lizi@beeski.com>
2019-09-11 18:53:40 +0100

    Genetic Algorithm doing path finding

---
+++ GeneticAlgorithms/Assets/Scripts/Genome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+[Serializable]
+public class Genome
+{
+    public BitArray Bits { get; set; }
+
+    public double Fitness { get; set; }
+
+    public Genome( int numBits )
+    {
+        Bits = new BitArray(numBits);
+        for (int count = 0; count < numBits; ++count)
+        {
+            Bits.Set(count, UnityEngine.Random.value > 0.5f);
+        }
+        Fitness = 0.0;
+    }
+}
